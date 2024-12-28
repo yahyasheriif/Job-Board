2753c77338caea538ec9cfd4a4ac43bb49108de5
yahya sheriif <yahyasabdallah21@gmail.com>
2024-12-28 22:57:16 +0200

    bla bla

--- job/models.py
+++ job/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 class Job(models.Model):
     title = models.CharField(max_length=100)
     JOB_TYPE = (
@@ -8,18 +8,21 @@ class Job(models.Model):
     )
     job_type = models.CharField(max_length=100,choices=JOB_TYPE)
     description= models.TextField(max_length=1000)
-    punlished_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(auto_now=True,auto_now_add=False)
     vacancy= models.IntegerField(default=1)
     salary= models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
-    
+    slug = models.SlugField(blank=True, null=True)
+
     def upload_img():
         return "jobs/%y/%m/%d"
     image = models.ImageField(upload_to=upload_img ())
     def __str__(self):
         return self.title
-    
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args,**kwargs)
 class Category(models.Model):
     name= models.CharField(max_length=25)
     def __str__(self):
